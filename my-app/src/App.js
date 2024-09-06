570e2b4d3b367a875df32c40df55e6c460dd4318
Matheus Quirino <matheusf730@gmail.com>
2024-09-06 15:26:13 -0300

    Up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SayMyName from './componets/SayMyName'
 import Pessoa from './componets/Pessoa'
 import Frase from './componets/Frase'
+import List from './componets/List'
 
 
 function App() {
@@ -15,7 +16,12 @@ function App() {
       <SayMyName nome = "Matheus" />
       <SayMyName nome = "Jao" />
       <SayMyName nome = {nome} />
-      <Pessoa nome="Irineu" idade= "38" profissao= "Garoto de Programa" foto= "https://via.placeholder.com/150" />
+      <Pessoa nome="Irineu" 
+      idade= "38" 
+      profissao= "Garoto de Programa" 
+      foto= "https://via.placeholder.com/150" 
+      />
+      <List />
     </div>
   )
 }
